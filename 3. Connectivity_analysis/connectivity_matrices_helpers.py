1569e845e7d56745174fb530d90b90fbd3a6c1da
jboulanger91 <jonathan.boulanger.weill@gmail.com>
2025-12-05 11:57:16 +0100

    matrix coloring fixes

--- 3. Connectivity_analysis/connectivity_matrices_helpers.py
+++ 3. Connectivity_analysis/connectivity_matrices_helpers.py
@@ -76,7 +76,7 @@ COLOR_CELL_TYPE_DICT_LR: Dict[str, Tuple[float, float, float, float]] = {
     "axon_rostral_left": (1.0, 1.0, 1.0, 0.7),
     "axon_rostral_right": (1.0, 1.0, 1.0, 0.7),
     "axon_caudal_left": (0.0, 0.0, 0.0, 0.7),
-    "axon_caudal_right": (0.7, 0.7, 0.7, 0.7),
+    "axon_caudal_right": (0.0, 0.0, 0.0, 0.7),
 }
 
 # -------------------------------------------------------------------------
