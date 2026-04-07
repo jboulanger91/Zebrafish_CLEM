a715790ba47c50658465b7e5504504a915d83009
Roberto <roberto.garza@mail.polimi.it>
2026-04-07 22:06:59 +0200

    Roberto | implement fig_supp_bestmodels

---
+++ 5. Connectome_constrained_network_modeling/figures/fig_supp_bestmodels.py
@@ -0,0 +1,207 @@
+from pathlib import Path
+from dotenv import dotenv_values
+
+import torch
+import numpy as np
+
+# Manually add root path for imports to improve interoperability
+import sys; sys.path.insert(0, "..")
+
+from style import RNNDSStyle
+from utils.services.rnn_service import RNNService
+from utils.figure_helper import Figure
+from utils.load_model import load_model
+
+# ================================================================
+# Env and paths
+# ================================================================
+env = dotenv_values()
+path_top_model = Path(env["PATH_MODELS"])
+path_save = Path(env["PATH_SAVE"])
+
+# ================================================================
+# Configurations
+# ================================================================
+i_trial = 2
+model_is_free_pop = True
+show_matrix_style = ["recorded"]  # ["recorded", "all"] as an alternative to show the full matrix as well
+
+# ================================================================
+# Plot configuration (layout, sizes, padding, etc.)
+# ================================================================
+style = RNNDSStyle()
+
+xpos_start = style.xpos_start
+ypos_start = style.ypos_start - 1
+xpos = xpos_start
+ypos = ypos_start
+
+plot_height = style.plot_height
+plot_height_small = plot_height / 2.5
+
+plot_width = style.plot_width
+plot_width_small = style.plot_width_small
+
+plot_size_matrix = style.plot_size_big * 1.2
+
+padding = style.padding / 2
+padding_big = style.padding * 2
+padding_vertical = style.padding
+
+palette = style.palette["neurons_5"]
+
+# ================================================================
+# Initialize figure container
+# ================================================================
+fig = Figure()
+
+loss_list = []
+model_list = []
+# Fetch models
+for path_model in path_top_model.glob("model_*.pt"):
+    model = load_model(path_model)
+    model_list.append(model)
+    loss_list.append(model.loss_mse)
+
+# Sort them by performance in training
+top_indices = np.argsort(loss_list)  # [:1]
+
+for i_m, i_m_sorted in enumerate(top_indices):
+    model = model_list[i_m_sorted]
+    # ----- Basic checks -----
+    if not hasattr(model, "xs"):
+        raise ValueError("model.xs not found. Run a forward pass before plotting.")
+    if not hasattr(model, "population_indices"):
+        raise ValueError("model.population_indices not found.")
+    # if not hasattr(model, "free_indices_by_pop") or not hasattr(model, "anchor_indices_by_pop"):
+    #     raise ValueError("model.free_indices_by_pop / anchor_indices_by_pop not found.")
+
+    # Activity vector
+    xs = model.xs  # (N, T, n_units)
+    if not torch.is_tensor(xs):
+        xs = torch.tensor(xs, dtype=torch.float32)
+    N, T, _ = xs.shape
+    n_units = model.n_units_hemi * 2
+    if model_is_free_pop:
+        n_units_X = model.nX
+
+    # Time vector
+    time = np.arange(T) * model.dt
+
+    # ================================================================
+    # Show matrices
+    # ================================================================
+    for matrix_style in show_matrix_style:
+        show_free_pop = True if model_is_free_pop and matrix_style == "all" else False
+
+        if model_is_free_pop and show_free_pop:
+            n_units_show = n_units + n_units_X
+            colormap = RNNDSStyle.cmap_list["neurons_5"]
+        else:
+            n_units_show = n_units
+            colormap = RNNDSStyle.cmap_list["neurons_4"]
+
+        offset_hemisphere = model.nA + model.nB + model.nC + model.nD
+        neuron_identity_array = np.concatenate(
+            (np.zeros((model.nA, 1)), np.ones((model.nB, 1)), 2 * np.ones((model.nC, 1)),
+             3 * np.ones((model.nD, 1)),
+             np.zeros((model.nA, 1)), np.ones((model.nB, 1)), 2 * np.ones((model.nC, 1)),
+             3 * np.ones((model.nD, 1))))
+        # Optionally add label for free population neurons and normalize
+        if model_is_free_pop and show_free_pop:
+            neuron_identity_array = np.concatenate((neuron_identity_array, 4 * np.ones((model.nX, 1)))) / 4
+        else:
+            neuron_identity_array /= 3
+
+        U = model.U().detach().numpy()
+        mask_U = model.mask_U.detach().numpy()
+        W = model.W().detach().numpy().T
+        mask_W = (model.mask_W * model.signs).detach().numpy().T
+
+        if not show_free_pop:
+            U = U[:n_units_show]
+            mask_U = mask_U[:n_units_show]
+            W = W[:n_units_show, :n_units_show]
+            mask_W = mask_W[:n_units_show, :n_units_show]
+        grid_pop = np.array([model.nA, model.nA + model.nB, model.nA + model.nB + model.nC,
+                             model.nA + model.nB + model.nC + model.nD,
+                             offset_hemisphere + model.nA, offset_hemisphere + model.nA + model.nB,
+                             offset_hemisphere + model.nA + model.nB + model.nC])
+        if model_is_free_pop and show_free_pop:
+            grid_pop = np.concatenate([grid_pop, [offset_hemisphere + model.nA + model.nB + model.nC + model.nD]])
+
+        mask_U = model.mask_U
+        _, _, ypos = RNNService.plot_connectivity(mask_W, U=mask_U, neuron_identity_array=neuron_identity_array,
+                                                     grid_pop=grid_pop,
+                                                     fig=fig, xpos=xpos, ypos=ypos, plot_size_matrix=plot_size_matrix,
+                                                     padding=padding, value_lim=[-1, 1],
+                                                     plot_title=f"{matrix_style.capitalize() + '\n'}Mask W",
+                                                     cmap=colormap, show_colorbar=i_m == len(top_indices)-1)
+
+        ypos -= plot_size_matrix + padding * 1.5
+        _, _, ypos = RNNService.plot_connectivity(W, U=U, neuron_identity_array=neuron_identity_array,
+                                                     grid_pop=grid_pop,
+                                                     fig=fig, xpos=xpos, ypos=ypos, plot_size_matrix=plot_size_matrix,
+                                                     padding=padding, value_lim=[-1, 1], cmap=colormap, show_colorbar=i_m == len(top_indices)-1)
+
+    ypos -= plot_size_matrix + padding*1.5
+
+    # Store reference position after matrix
+    xpos_start_here = xpos
+    ypos_start_here = ypos
+
+    # Select the trial (the example shown in training)
+    x_trial = xs[i_trial].detach().cpu().numpy()  # (T, n_units)
+
+    # store info about populations
+    population_indices = model.population_indices
+    free_by_pop = [model.idx_X]  # model.free_indices_by_pop
+    free_indices = model.idx_X  # model.free_indices_by_pop
+    anchor_by_pop = population_indices  # model.anchor_indices_by_pop
+    n_pops = len(population_indices)
+    if len(population_indices) == 0:
+        raise ValueError("No valid populations to plot were provided.")
+
+    # ================================================================
+    # Show single-neuron activity
+    # ================================================================
+    # ----- Create figure -----
+    offset_side = 0
+    for i_pop, i_neurons_in_pop in enumerate(population_indices):
+        if i_pop == 4:
+            offset_side = 1
+            xpos = xpos_start_here + plot_width+padding
+            ypos = ypos_start_here
+        # Find free neuron traces
+        # free_indices = np.array(free_by_pop[i_pop], dtype=int)
+        free_traces = x_trial[:, free_indices]  # (T, n_free)
+
+        # Find anchor neuron traces
+        anchor_indices = np.array(anchor_by_pop[i_pop], dtype=int)
+        anchor_traces = x_trial[:, anchor_indices]  # (T, n_anchor)
+
+        plot_pop_n = fig.create_plot(plot_title=f"{RNNDSStyle.population_name_list[i_pop]}" if i_m == 0 else None,
+                                     xpos=xpos, ypos=ypos, plot_width=plot_width, plot_height=plot_height,
+                                     xmin=0, xmax=np.max(time),  # xl="Time (s)", xticks=[0, 20, 60, 80],
+                                     ymin=0, ymax=8,  # yl=f"Model {i_m}\nActivity" if i_pop == 0 else None, yticks=[0, 4, 8] if i_pop == 0 else None,
+                                     vspans=[[20, 60, "k", 0.1]])
+        if i_pop == 0:
+            plot_pop_n.draw_line(np.ones(2) * 25, [6, 7], lc="k")
+            plot_pop_n.draw_text(0, 5, r"$\Delta$F/F" + "\n1")
+        elif i_pop == len(population_indices) - 2:
+            plot_pop_n.draw_line([1, 6], np.ones(2)*6, lc="k")
+            plot_pop_n.draw_text(1, 4, f"5\nTime (s)")
+        ypos -= plot_height + padding*1.5
+
+        color = palette[-1] if i_pop == len(population_indices)-1 else palette[i_pop % 4]
+        plot_pop_n.draw_line(time, anchor_traces, lc=color)
+        # plot_pop_n.draw_line(time, free_traces, lc="k", label="Free neurons" if i_m == 0 and i_pop==len(population_indices)-1 else None)
+
+    xpos += plot_size_matrix * 4/5
+    ypos = ypos_start
+
+# ================================================================
+# Save final figure
+# ================================================================
+path_save.mkdir(parents=True, exist_ok=True)
+fig.save(path_save / f"figure_supp_bestmodels.pdf", open_file=False, tight=style.page_tight)
