Roberto | parameterize fixed_U in RNNFixedConnectivity pt3; fix computation of W with 1D beta

5. Connectome_constrained_network_modeling/analysis/load_synapse_matrix.py
@@ -80,7 +80,7 @@ def load_synapse_matrix(csv_path, drop_axons=True, drop_myelinated=True):
     idx_to_id = {i: label for i, label in enumerate(df_clean.index)}
     id_to_idx = {label: i for i, label in idx_to_id.items()}
 
-    return matrix, idx_to_id, id_to_idx, df_clean, idx_side_change, axon_values
+    return matrix, axon_values, idx_to_id, id_to_idx, df_clean, idx_side_change
 
 def process_synapse_matrix(W_raw, idx_to_id, idx_side_change):
     # normalize W_raw
@@ -136,18 +136,19 @@ def process_synapse_matrix(W_raw, idx_to_id, idx_side_change):
     return W, dict_neurons
 
 def get_W(path_W_csv, do_symmetry_transform=False):
-    W_raw, idx_to_id, _, _, idx_side_change, axon_values = load_synapse_matrix(path_W_csv)
+    W_raw, U, idx_to_id, _, _, idx_side_change = load_synapse_matrix(path_W_csv)
     W, _dict_neurons = process_synapse_matrix(W_raw, idx_to_id, idx_side_change)
     if do_symmetry_transform:
-        W, _dict_neurons = symmetry_transform(W, _dict_neurons)
+        W, U_sim, _dict_neurons = symmetry_transform(W, U, _dict_neurons)
     dict_neurons = {"neurons": _dict_neurons,
                     "W": W,
+                    "U": U,
                     "idx_side_change": idx_side_change,
                     "is_symmetry_transformed": do_symmetry_transform,
                     "symmetry_transform": ~do_symmetry_transform,}
     return W, dict_neurons
 
-def symmetry_transform(W, dict_neurons):
+def symmetry_transform(W, U, dict_neurons):
     # accept L or R as reference side depending on which one is the smallest one
     # (for which we have all information available to build a symmetric matrix)
     if len(dict_neurons[ConfigurationRNN.SIDE_LEFT]["idx_list"]) <= len(dict_neurons[ConfigurationRNN.SIDE_RIGHT]["idx_list"]):
@@ -169,7 +170,10 @@ def symmetry_transform(W, dict_neurons):
     W_sim[np.ix_(idx_OTHER[:n_neurons_side], idx_OTHER[:n_neurons_side])] = W[np.ix_(idx_REF, idx_REF)]
     W_sim[np.ix_(idx_REF, idx_OTHER[:n_neurons_side])] = W[np.ix_(idx_OTHER[:n_neurons_side], idx_REF)]
 
-    return W_sim, dict_neurons
+    # define symmetric U mirroring reference side
+    U_sim = np.repeat(U[idx_REF], 2)
+
+    return W_sim, U_sim, dict_neurons
 
 # if __name__ == "__main__":
 #     csv_path = r"C:\Users\Roberto\Desktop\left-right_matrix_e-i_raster_native.csv"

5. Connectome_constrained_network_modeling/model/core/RNNFixedConnectivity.py
@@ -134,12 +134,19 @@ class RNNFixedConnectivity(nn.Module):
             self.register_buffer("mask_U", block_mask(self.n_units, input_dim, 1, seed))
             self.register_buffer("U_raw", fixed_U)
 
+    def process_beta(self):
+        if self.beta.dim() > 0:
+            # map the 8 population-related beta values (associated to the input into each population) into a full matrix
+            beta = torch.concat([torch.ones((len(pop), self.n_units)) * self.beta.detach().clone()[i_pop]
+                                 for i_pop, pop in enumerate(self.population_indices)])
+        else:
+            beta = self.beta
+        return beta
+
     # ---------- transforms ----------
     def W(self):
-        if self.beta.dim() > 0:
-            W = self.W_norm @ self.beta
-        else:
-            W = self.W_norm * self.beta
+        beta = self.process_beta()
+        W = self.W_norm * beta
         return W
 
     def U(self):

5. Connectome_constrained_network_modeling/model/train_rnn_fixed_connectivity.py
@@ -23,6 +23,8 @@ if __name__ == '__main__':
     fit_model = True
 
     # Generate a 1D input and target
+    n_beta = 8
+    label_U = "free"  # "uniform"  # "connectome"
     activation = "softplus"
     dt = 0.01
     duration_rest_start = 20
@@ -72,7 +74,17 @@ if __name__ == '__main__':
         n_units_RMON = dict_neurons["neurons"][ConfigurationRNN.SIDE_RIGHT]["MON"]["n_neurons"]
         n_units_RsMI = dict_neurons["neurons"][ConfigurationRNN.SIDE_RIGHT]["sMI"]["n_neurons"]
         n_units = W_norm.shape[0]
-        rnn = RNNFixedConnectivity(W_norm, dict_neurons, n_beta=1, fixed_U=None,
+
+        if label_U is None or label_U == "free":
+            fixed_U = None
+        elif label_U == "uniform":
+            fixed_U = torch.ones(n_units) / n_units
+        elif label_U == "connectome":
+            fixed_U = dict_neurons["U"]
+        else:
+            raise NotImplementedError
+
+        rnn = RNNFixedConnectivity(W_norm, dict_neurons, n_beta=n_beta, fixed_U=fixed_U,
                                    sparsity_U=1, tau=tau_neuron, dt=dt,
                                    seed=seed, activation=activation, clamp_weights_min=1e-2)
 
@@ -193,7 +205,7 @@ if __name__ == '__main__':
             "class_name": type(rnn).__name__,
         }
 
-        label_model = f"RNNFixedConnectivity_neurons{n_units}_input{n_input_signal}step_{activation}"
+        label_model = f"RNNFixedConnectivity_neurons{n_units}_nbeta{n_beta}_U{label_U}"
         label_model_instance = f"{datetime.today().strftime('%Y-%m-%d_%H-%M-%S')}"
         path_save_model = path_save / label_model
         path_save_model.mkdir(parents=True, exist_ok=True)